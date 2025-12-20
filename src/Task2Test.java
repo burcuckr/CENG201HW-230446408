9bef53ff715496c49137ad2c503628f7e8fe91c2
burcuckr <s230446408@stu.thk.edu.tr>
2025-12-20 21:53:55 +0300

    task2

---
+++ src/Task2Test.java
@@ -0,0 +1,22 @@
+public class Task2Test {
+    public static void main(String[] args){
+        TreatmentQueue  treatmentRequests = new TreatmentQueue();
+
+        treatmentRequests.enqueue(new TreatmentRequest(1));
+        treatmentRequests.enqueue(new TreatmentRequest(2));
+        treatmentRequests.enqueue(new TreatmentRequest(3));
+        treatmentRequests.enqueue(new TreatmentRequest(4));
+        treatmentRequests.enqueue(new TreatmentRequest(5));
+        treatmentRequests.enqueue(new TreatmentRequest(6));
+        treatmentRequests.enqueue(new TreatmentRequest(7));
+        treatmentRequests.enqueue(new TreatmentRequest(8));
+
+        treatmentRequests.printQueue(); //After removing an elements
+
+        System.out.println(treatmentRequests.dequeue());
+        System.out.println(treatmentRequests.dequeue());
+        System.out.println(treatmentRequests.dequeue());
+
+        treatmentRequests.printQueue();
+    }
+}
